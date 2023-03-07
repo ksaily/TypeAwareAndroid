5b99102b155e4cf8d92e81f3fdd8a1d9e243f37d
ksaily <katri.saily@elisanet.fi>
2023-03-07 21:46:51 +0200

    Update files

---
+++ app/src/main/java/com/example/testing/ui/menu/DateFragment.kt
@@ -0,0 +1,82 @@
+package com.example.testing.ui.menu
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import com.example.testing.R
+import com.example.testing.databinding.FragmentDateBinding
+import com.example.testing.databinding.FragmentHomeBinding
+import com.example.testing.ui.view.DateViewModel
+import com.example.testing.utils.Utils.Companion.currentDate
+import com.example.testing.utils.Utils.Companion.getFromFirebase
+import com.example.testing.utils.Utils.Companion.getNextDateString
+import com.example.testing.utils.Utils.Companion.getPreviousDateString
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class DateFragment : Fragment(R.layout.fragment_date) {
+
+    private var _binding: FragmentDateBinding? = null
+    private val binding get() = _binding!!
+    //Initialize the viewmodel
+    private val viewModel: DateViewModel by viewModels()
+
+    companion object {
+        var chosenDate: String = ""
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentDateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //binding.currentDate.text = "Today"
+        //chosenDate = currentDate
+        binding.arrowLeft.setOnClickListener {
+            viewModel.previousDay()
+            binding.currentDate.text = viewModel.checkDate()
+            //Get data from Firebase
+            getFromFirebase(viewModel.selectedDate.value.toString())
+            //Set up LiveData listener in Home and chart fragments:
+            //Changes in selectedDate -> Update UI
+        }
+
+        binding.arrowRight.setOnClickListener {
+            viewModel.nextDay()
+            binding.currentDate.text = viewModel.checkDate()
+            getFromFirebase(viewModel.selectedDate.value.toString())
+        }
+
+        viewModel.isToday.observe(viewLifecycleOwner
+        ) { // Check if date is today, if yes, remove right arrow
+            binding.arrowRight.isVisible = viewModel.isToday.value != true
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("DateFragment", "DateFragment destroyed!")
+    }
+
+}
---
+++ app/src/main/java/com/example/testing/utils/KeyboardStats.kt
@@ -0,0 +1,8 @@
+package com.example.testing.utils
+
+data class KeyboardStats(
+    val date: String?,
+    val timeWindow: String?,
+    val errors: Double,
+    val speed: Double
+)
